a9b5f55fddc8a5fe3d514904158a328c5d7d3ce3
eliasderbewbelay <eliasderbew1@gmail.com>
2026-03-08 23:22:45 +0300

    page routing

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,14 @@
+import { Link } from "expo-router";
 import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View className="flex-1 justify-center items-center bg-slate-900">
-      <Text className="text-4xl font-bold text-white mb-4">Welcome</Text>
-      <Text className="text-lg text-blue-400">NativeWind is working! 🎉</Text>
+    <View className="flex-1 justify-center items-center">
+      <Text className="font-bold text-primary-400 text-5xl">Welcome!</Text>
+      <Text className="font-bold text-accent-200">
+        This is my first React Native app
+      </Text>
+      <Link href="/onboarding">onboarding</Link>
     </View>
   );
 }
---
+++ app/onboarding.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function onboarding() {
+  return (
+    <View className="flex-1 justify-center items-center">
+      <Text>This is on boarding page</Text>
+    </View>
+  );
+}
