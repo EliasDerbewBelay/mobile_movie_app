ceb7e2620dc63845320ba66c9eb668470ce299fe
eliasderbewbelay <eliasderbew1@gmail.com>
2026-03-10 00:31:52 +0300

    feat/clean ui of latest movies

--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -1,5 +1,6 @@
+import { icons } from "@/constants/icons";
 import { Link } from "expo-router";
-import { Image, Text, TouchableOpacity } from "react-native";
+import { Image, Text, TouchableOpacity, View } from "react-native";
 
 export default function MovieCard({
   id,
@@ -21,7 +22,26 @@ export default function MovieCard({
           resizeMode="cover"
         />
 
-        <Text className="text-sm font-bold text-white mt-2">{title}</Text>
+        <Text className="text-sm font-bold text-white mt-2" numberOfLines={1}>
+          {title}
+        </Text>
+
+        <View className="flex-row items-center justify-start gap-x-1">
+          <Image source={icons.star} className="size-4" />
+          <Text className="text-xs text-white font-bold uppercase">
+            {Math.round(vote_average / 2)}
+          </Text>
+        </View>
+
+        <View className="flex-row items-center justify-between">
+          <Text className="text-xs text-light-300 font-medium mt-1">
+            {release_date?.split("-")[0]}
+          </Text>
+
+          {/* <Text className="text-xs font-medium text-light-100 uppercase">
+            Movie
+          </Text> */}
+        </View>
       </TouchableOpacity>
     </Link>
   );
